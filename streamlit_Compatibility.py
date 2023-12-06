95c85115bb6f38e3ba2fa366c26c7cd717c33a75
Minladaa <6640218122@student.chula.ac.th>
2023-12-06 14:41:17 +0700

    Update streamlit_Compatibility.py

--- streamlit_Compatibility.py
+++ streamlit_Compatibility.py
@@ -1,5 +1,32 @@
 import streamlit as st
 
-st.header("Compatibility")
-"An example"
+st.cache_data.clear()
+
+if "openai_api_key" not in st.session_state:
+    st.session_state.openai_api_key = ""
+
+openai.api_key = st.session_state.openai_api_key
+
+if "text_error" not in st.session_state:
+    st.session_state.text_error = None
+
+if "text" not in st.session_state:
+    st.session_state.text = None
+
+if "n_requests" not in st.session_state:
+    st.session_state.n_requests = 0
+
+with st.sidebar:
+    api_key_form = st.form(key="api_key_form")
+    openai_api_key = api_key_form.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+    api_key_form_submitted = api_key_form.form_submit_button("Submit")
+
+    if api_key_form_submitted:
+        st.session_state.openai_api_key = openai_api_key
+        openai.api_key = st.session_state.openai_api_key
+        st.success("Your OpenAI API key was saved successfully!")
+
+
+
+st.title("Compatibility")
 
